d47542e69527d45736429a58081844c5e9aee2f1
Andriel Ribeiro <andriel_ribeiro@estudante.sesisenai.org.br>
2025-08-12 12:31:50 -0300

    add>create

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Padaria bumba meu boi </title>
+     <link rel="stylesheet" href="style.css">
+      <a href="read.php">Ver registros</a>
 </head>
 <body>
     

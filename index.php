0e8259a9588769a4ad24ca88cdc01516d4c4a529
Andriel Ribeiro <andriel_ribeiro@estudante.sesisenai.org.br>
2025-08-12 12:48:02 -0300

    add>arruamando erro

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Padaria bumba meu boi </title>
      <link rel="stylesheet" href="style.css">
-      <a href="read.php">Ver registros</a>
+      <a href='read.php'>Ver registros</a>
       <a href='create.php'>Inserir novo Registro</a>
-      <a href='update.php?id'>Editar<a>
+      <a href='update.php?'>Editar<a>
       <a href='delete.php?'>Excluir<a>
 </head>
 <body>

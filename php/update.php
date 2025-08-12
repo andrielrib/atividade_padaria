35d38eb88fe799d17dd00b52ba15ade1cc0d8146
Arthur Farias <arthur_s_farias@estudante.sesisenai.org.br>
2025-08-12 11:42:12 -0300

    add - link

--- php/update.php
+++ php/update.php
@@ -36,6 +36,7 @@ $row = $result -> fetch_assoc();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>update</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>

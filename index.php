b1391ce746c54109005fa1fb7cf33ec45bfd5ac5
Andriel Ribeiro <andriel_ribeiro@estuante.sesisenai.org.br>
2025-08-14 09:36:42 -0300

    add>andriel burro

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 <h1>Padaria Bumba meu Pão</h1>
 <ul>
-    <li><a href="create.php">Cadastrar Produto</a></li>
-    <li><a href="read.php">Listar Produtos</a></li>
+    <li><a href="php/create.php">Cadastrar Produto</a></li>
+    <li><a href="php/read.php">Listar Produtos</a></li>
 </ul>

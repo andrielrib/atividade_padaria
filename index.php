bb93c1b1c099d9622bfae628fe8e300c69154cda
Arthur Farias <arthur_s_farias@estudante.sesisenai.org.br>
2025-08-15 08:35:04 -0300

    add - adsfsdf

--- index.php
+++ index.php
@@ -11,15 +11,9 @@
 
     <a class="btn" href="php/create.php">Cadastrar Produto</a>
     <a class="btn" href="php/read.php">Ver Produtos</a>
-<<<<<<< HEAD
-    <a class="btn" href="php/read.php">Atualizar Produtos</a>
-    <a class="btn" href="php/delete.php">Deletar Produtos</a>
-    
-=======
     <a class="btn" href="php/update.php">Atualizar Produtos</a> 
     <a class="btn" href="php/delete.php">Deletar Produtos</a> 
 
->>>>>>> 42afc11b63a2383957b104ccd26938c430593ca8
 </div>
 
 </body>
